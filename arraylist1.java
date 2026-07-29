15df53407a9dfe1c9a3ea96e8ec2e174578157c0
Durgadevi T <145424482+Durgadevi05-dd@users.noreply.github.com>
2026-07-29 10:27:36 +0530

    Add files via upload

---
+++ arraylist1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class arraylist1
+{
+public static void main(String[] args)
+{
+ArrayList<String> names=new ArrayList<> ();
+names.add("Anu");
+names.add("Ravi");
+names.add("Kumar");
+System.out.println(names);
+System.out.println(names.get(1));
+names.set(1,"Arun");
+names.remove("Anu");
+System.out.println(names);
+}
+}
